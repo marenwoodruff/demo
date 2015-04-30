7332b3a15e86cd2cc73010e4a219f517e8cf9938
Maren Woodruff <wren403@gmail.com>
2015-04-30 13:50:19 -0400

    Added bootstrap styles to devise pages + correct links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
   devise_for :users
   resources :products
 
@@ -10,7 +12,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
