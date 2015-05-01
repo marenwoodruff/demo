ea06a8bdb17adee97d07871a0119da9d74438acf
Maren Woodruff <wren403@gmail.com>
2015-04-30 21:06:23 -0400

    Added association relationship between Users and Products. Changed models and index view.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
+	belongs_to :user
+
 	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :image, :attachment_presence => true
+
+  validates :name, :description, :price, presence: true
+
 end
