4add5c388ca35c6dacd4909b389a58653ddc5c0f
Maren Woodruff <wren403@gmail.com>
2015-05-07 08:38:04 -0400

    added product_image model + associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,12 @@ class Product < ActiveRecord::Base
 	belongs_to :user
 	has_many :product_images, :dependent => :destroy
 
-	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  validates :image, :attachment_presence => true
+	# has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png"
+ #  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+ #  validates :image, :attachment_presence => true
+
+  accepts_nested_attributes_for :product_images, :reject_if => lambda { |t| t['product_image'].nil? }, :allow_destroy => true
+  # allows you to manage the product_image model through the products model; won't show if there aren't any product images
 
   validates :name, :description, :price, presence: true
 
--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,2 +1,7 @@
 class ProductImage < ActiveRecord::Base
+	belongs_to :products
+
+	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :image, :attachment_presence => true
 end
