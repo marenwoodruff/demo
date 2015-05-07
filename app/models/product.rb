c78df80e772aba7aac1cc7b41d847d1fdf624be7
Maren Woodruff <wren403@gmail.com>
2015-05-06 20:47:12 -0400

    added product_image model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :user
+	has_many :product_images, :dependent => :destroy
 
 	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430235813) do
+ActiveRecord::Schema.define(version: 20150507004455) do
+
+  create_table "product_images", force: :cascade do |t|
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
