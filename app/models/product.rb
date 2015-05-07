75aeaf9e70b8dbb6f0762abbdeeacefa83721566
Maren Woodruff <wren403@gmail.com>
2015-05-07 17:18:19 -0400

    added paperclip config info into product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,14 @@ class Product < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, 
-		:default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png",
-		:s3_credentials => "#{RAILS_ROOT}/config/initializers/paperclip.rb"
+		:path => "/:id/:style.:filename",
+		:storage => :s3,
+		:s3_credentials => {
+			:bucket => ENV['S3_BUCKET_NAME'],
+			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+		},
+		:default_url => "none"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :image, :attachment_presence => true
