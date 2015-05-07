bff0f6fd878813cd420d468fbda14bfed97b9568
Maren Woodruff <wren403@gmail.com>
2015-05-07 10:45:53 -0400

    changed product model so that paperclip/s3 used for prod and development

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,15 @@
 class Product < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png"
+	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, 
+		:default_url => "https://s3-us-west-2.amazonaws.com/tradedemos/missing.png",
+		:storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :image, :attachment_presence => true
 
