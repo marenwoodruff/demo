191158387e242cd7b4b86418d40fe9acb73e35d8
Maren Woodruff <wren403@gmail.com>
2015-05-08 10:05:16 -0400

    added paperclip code back to config/paperclip.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,14 +2,6 @@ class Product < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, 
-		:path => "/:id/:style.:filename",
-		:storage => :s3,
-		:s3_protocol => 'https',
-		:s3_credentials => {
-			:bucket => ENV['S3_BUCKET_NAME'],
-			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-		},
 		:default_url => "none"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
