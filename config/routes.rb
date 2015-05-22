d7b116874755fc9d0b5a79f46e3d0cb92af5b53b
Maren Woodruff <wren403@gmail.com>
2015-05-22 16:11:43 -0400

    added bootstrap styling to all forms. new users/registrations controller to take out current_password from user edit page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'products#index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :products
 
   get 'pages/about'
