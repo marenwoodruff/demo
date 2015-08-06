33214b4921a13e41c1ed23b7ce9df104a50817dc
Maren Woodruff <wren403@gmail.com>
2015-08-06 16:08:19 -0400

    changing code to fix no customer error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,23 +30,42 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @plan = Plan.find(params[:order][:plan_id])
-    @order.buyer_id = current_user.id
+    # @order.buyer_id = current_user.id
 
-    respond_to do |format|
-      if @order.save_with_payment
-        charge = Stripe::Charge.create(
+    customer = Stripe::Customer.create(
+                  :email => current_user.email,
+                  :plan => @plan_id,
+                  :card  => params[:stripe_card_token]
+    )
+
+    charge = Stripe::Charge.create(
                   :amount => @plan.price.to_i * 100,
                   :currency => "usd",
-                  :source => params[:stripe_card_token],
+                  :customer => customer.id,
                   :description => "Test Charge"
-        )
-        format.html { redirect_to root_url, notice: 'Thank you for subscribing.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { redirect_to back, notice: 'Not yet working.' }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
+    )
+
+    redirect_to root_url, notice: 'Thank you for subscribing.'
+    
+  rescue Stripe::CardError => e
+    logger.error "Stripe error while creating customer: #{e.message}"
+    errors.add :base, "There was a problem with your credit card."
+
+    # respond_to do |format|
+    #   if @order.save_with_payment
+    #     charge = Stripe::Charge.create(
+    #               :amount => @plan.price.to_i * 100,
+    #               :currency => "usd",
+    #               :source => params[:stripe_card_token],
+    #               :description => "Test Charge"
+    #     )
+    #     format.html { redirect_to root_url, notice: 'Thank you for subscribing.' }
+    #     format.json { render :show, status: :created, location: @order }
+    #   else
+    #     format.html { redirect_to back, notice: 'There was an internal server error. Please be patient while we fix the issue. Thank you!' }
+    #     format.json { render json: @order.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   private
@@ -57,6 +76,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:address, :city, :state)
+      params.require(:order).permit(:address, :city, :state, :plan_id, :stripe_customer_token, :buyer_id, :seller_id, :product_id)
     end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -10,17 +10,17 @@ class Order < ActiveRecord::Base
 
   # validates_presence_of :plan_id
 
-  def save_with_payment
-    if valid?
-      customer = Stripe::Customer.create(email: buyer.email,
-        plan: plan_id, card: stripe_card_token)
+  # def save_with_payment
+  #   if valid?
+  #     customer = Stripe::Customer.create(email: buyer.email,
+  #       plan: plan_id, card: stripe_card_token)
 
-      self.stripe_customer_token = customer.id
-      save!
-    end
+  #     self.stripe_customer_token = customer.id
+  #     save!
+  #   end
 
-  rescue Stripe::InvalidRequestError => e
-    logger.error "Stripe error while creating customer: #{e.message}"
-    errors.add :base, "There was a problem with your credit card."
-  end
+  # rescue Stripe::InvalidRequestError => e
+  #   logger.error "Stripe error while creating customer: #{e.message}"
+  #   errors.add :base, "There was a problem with your credit card."
+  # end
 end
